0833e38547e59458dbef5751cfbde623a7426d7f
DevYasa <mhdyasir132@gmail.com>
2024-06-17 15:19:24 +0530

    updatee

--- my-express-app/Index.js
+++ my-express-app/Index.js
@@ -2,40 +2,52 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const cors = require('cors');
+const dotenv = require('dotenv');
 const Contact = require('./models/Contact');
+const { generateImageFromText } = require('./services/huggingfaceService');
+
+dotenv.config();
+
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-const imageRoutes = require('./routes/imageRoutes');
-app.use('/api', imageRoutes);
-
-// Replace with your actual MongoDB URI
-const mongoUri = 'mongodb://localhost:27017/PromptMage';
-
-mongoose.connect(mongoUri);
-
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => {
-  console.log('Connected to MongoDB');
-});
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
 
 app.post('/api/contact', async (req, res) => {
   const { name, email, phone, message } = req.body;
 
-  // Save to MongoDB
   const newContact = new Contact({ name, email, phone, message });
   try {
-    await newContact.save();
-    res.status(201).send({ success: true, message: 'Contact information saved successfully.' });
+    const savedContact = await newContact.save();
+    console.log('Saved contact:', savedContact);
+    res.status(200).send({ success: true, message: 'Contact saved successfully.' });
   } catch (error) {
     console.error('Error saving to MongoDB:', error);
     res.status(500).send({ success: false, message: 'An error occurred while saving the contact.' });
+  }
+});
+
+app.post('/api/generate-image', async (req, res) => {
+  const { prompt } = req.body;
+  console.log('Received request to generate image with prompt:', prompt);
+  try {
+    const imageUrl = await generateImageFromText(prompt);
+    console.log('Generated image URL:', imageUrl);
+    res.status(200).send(imageUrl);
+  } catch (error) {
+    console.error('Error generating image:', error);
+    res.status(500).send({ success: false, message: 'An error occurred while generating the image.' });
   }
 });
 
